c2824ce61ca3d638e78ad5855d405d2e90c22629
Amanda Aureliano <44812731+amandaureliano@users.noreply.github.com>
2022-07-08 18:07:13 -0300

    exemplos incompletos

--- exemploBackEnd.js
+++ exemploBackEnd.js
@@ -1,1 +1,15 @@
+EXEMPLOS
+
+let nome = "Amanda"; //string
+let idade = 23; // number
+let altura = 1.69; // number
+
+let nota;
+console.log(nota); // undefined
+
+nota = null;
+console.log(nota); // null
+
+console.log(`Minha altura é ${alturaEmMetros} metros e a minha idade é ${idade} anos`); // string template
+// resposta do terminal: Minha altura é 1.69 metros e a minha idade é 23 anos
 
